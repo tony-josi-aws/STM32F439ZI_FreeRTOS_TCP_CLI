fd7ff3d57e6f68119c079f7f0daabc68f552d0ed
tony-josi-aws <tonyjosi@amazon.com>
2023-12-13 11:28:26 +0530

    optionally add TCP zero copy

--- Lib/FreeRTOS-Plus-CLI/tcp_echo_client.c
+++ Lib/FreeRTOS-Plus-CLI/tcp_echo_client.c
@@ -3,12 +3,14 @@
 
 #define echoNUM_ECHO_CLIENTS				1
 #define echoTCP_ECHO_SERVER_PORT			5050
-#define configTCP_ECHO_SERVER_ADDR			"35.172.136.157"
+#define configTCP_ECHO_SERVER_ADDR			"192.168.0.149"
 
 /* The size of the buffers is a multiple of the MSS - the length of the data
 sent is a pseudo random size between 20 and echoBUFFER_SIZES. */
 #define echoBUFFER_SIZE_MULTIPLIER	( 3 )
 #define echoBUFFER_SIZES			( ipconfigTCP_MSS * echoBUFFER_SIZE_MULTIPLIER )
+
+#define USE_TCP_ZERO_COPY					1
 
 #if (ipconfigUSE_TCP != 0)
 
@@ -85,6 +87,8 @@ uint32_t ulRandomNumber;
 }
 
 /*-----------------------------------------------------------*/
+
+#if (USE_TCP_ZERO_COPY == 0)
 
 static void prvEchoClientTask( void *pvParameters )
 {
@@ -266,5 +270,217 @@ TickType_t xTimeOnEntering;
 		vTaskDelay( echoLOOP_DELAY );
 	}
 }
+
+
+
+#else /* USE_TCP_ZERO_COPY */
+
+static void prvEchoClientTask( void *pvParameters )
+{
+	Socket_t xSocket;
+	struct freertos_sockaddr xEchoServerAddress;
+	int32_t lLoopCount = 0UL;
+	const int32_t lMaxLoopCount = 10;
+	volatile uint32_t ulTxCount = 0UL;
+	BaseType_t xReceivedBytes, xReturned, xInstance;
+	BaseType_t lTransmitted, lStringLength;
+	char *pcTransmittedString, *pcReceivedString;
+	WinProperties_t xWinProps;
+	TickType_t xTimeOnEntering;
+
+	/* Fill in the buffer and window sizes that will be used by the socket. */
+	xWinProps.lTxBufSize = 6 * ipconfigTCP_MSS;
+	xWinProps.lTxWinSize = 3;
+	xWinProps.lRxBufSize = 6 * ipconfigTCP_MSS;
+	xWinProps.lRxWinSize = 3;
+
+	/* This task can be created a number of times.  Each instance is numbered
+	to enable each instance to use a different Rx and Tx buffer.  The number is
+	passed in as the task's parameter. */
+	xInstance = ( BaseType_t ) pvParameters;
+
+	/* Point to the buffers to be used by this instance of this task. */
+	pcTransmittedString = &( cTxBuffers[ xInstance ][ 0 ] );
+	pcReceivedString = &( cRxBuffers[ xInstance ][ 0 ] );
+
+	/* Echo requests are sent to the echo server.  The address of the echo
+	server is configured by the constants configECHO_SERVER_ADDR0 to
+	configECHO_SERVER_ADDR3 in FreeRTOSConfig.h. */
+	xEchoServerAddress.sin_port = FreeRTOS_htons( echoTCP_ECHO_SERVER_PORT );
+
+	#if defined( ipconfigIPv4_BACKWARD_COMPATIBLE ) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 0 )
+	{
+		xEchoServerAddress.sin_address.ulIP_IPv4 = FreeRTOS_inet_addr( configTCP_ECHO_SERVER_ADDR );
+	}
+	#else
+	{
+		xEchoServerAddress.sin_addr = FreeRTOS_inet_addr( configTCP_ECHO_SERVER_ADDR );
+	}
+	#endif /* defined( ipconfigIPv4_BACKWARD_COMPATIBLE ) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 0 ) */
+
+	xEchoServerAddress.sin_family = FREERTOS_AF_INET;
+
+	for( ;; )
+	{
+		/* Create a TCP socket. */
+		xSocket = FreeRTOS_socket( FREERTOS_AF_INET, FREERTOS_SOCK_STREAM, FREERTOS_IPPROTO_TCP );
+		configASSERT( xSocket != FREERTOS_INVALID_SOCKET );
+
+		/* Set a time out so a missing reply does not cause the task to block
+		indefinitely. */
+		FreeRTOS_setsockopt( xSocket, 0, FREERTOS_SO_RCVTIMEO, &xReceiveTimeOut, sizeof( xReceiveTimeOut ) );
+		FreeRTOS_setsockopt( xSocket, 0, FREERTOS_SO_SNDTIMEO, &xSendTimeOut, sizeof( xSendTimeOut ) );
+
+		/* Set the window and buffer sizes. */
+		FreeRTOS_setsockopt( xSocket, 0, FREERTOS_SO_WIN_PROPERTIES, ( void * ) &xWinProps,	sizeof( xWinProps ) );
+
+		/* Connect to the echo server. */
+		if( FreeRTOS_connect( xSocket, &xEchoServerAddress, sizeof( xEchoServerAddress ) ) == 0 )
+		{
+			ulConnections[ xInstance ]++;
+
+			/* Send a number of echo requests. */
+			for( lLoopCount = 0; lLoopCount < lMaxLoopCount; lLoopCount++ )
+			{
+				char *pcTxString;
+				BaseType_t xAvlSpace = 0;
+				uint8_t *pucTCPZeroCopyStrmBuffer;
+				size_t uxDataLength;
+
+				pucTCPZeroCopyStrmBuffer = FreeRTOS_get_tx_head( xSocket, &xAvlSpace );
+
+				/* If there is enough space in the stream buffer, write the data directly to the stream
+				buffer to use zero copy. */
+				if( xAvlSpace > 64 )
+				{
+					pcTxString = pucTCPZeroCopyStrmBuffer;
+				}
+				else
+				{
+					pcTxString = pcTransmittedString;
+				}
+
+				/* Add in some unique text at the front of the string. */
+				sprintf( pcTxString, "TxRx message number %u", ulTxCount );
+				ulTxCount++;
+				uxDataLength = strlen(pcTxString);
+
+				/* Are we using TCP zero copy? If yes, set the buffer to NULL */
+				if(pcTxString != pcTransmittedString)
+				{
+					pcTxString = NULL;
+				}
+
+				/* Send the string to the socket. */
+				lTransmitted = FreeRTOS_send(	xSocket,						/* The socket being sent to. */
+												( void * ) pcTxString,	/* The data being sent. */
+												uxDataLength,					/* The length of the data being sent. */
+												0 );							/* No flags. */
+
+				if( lTransmitted < 0 )
+				{
+					/* Error? */
+					break;
+				}
+
+				/* Clear the buffer into which the echoed string will be
+				placed. */
+				memset( ( void * ) pcReceivedString, 0x00, echoBUFFER_SIZES );
+				xReceivedBytes = 0;
+
+				/* Receive data echoed back to the socket. */
+				while( xReceivedBytes < lTransmitted )
+				{
+					xReturned = FreeRTOS_recv( xSocket,								/* The socket being received from. */
+											&( pcReceivedString[ xReceivedBytes ] ),/* The buffer into which the received data will be written. */
+											 lStringLength - xReceivedBytes,		/* The size of the buffer provided to receive the data. */
+											 0 );									/* No flags. */
+
+					if( xReturned < 0 )
+					{
+						/* Error occurred.  Latch it so it can be detected
+						below. */
+						xReceivedBytes = xReturned;
+						break;
+					}
+					else if( xReturned == 0 )
+					{
+						/* Timed out. */
+						break;
+					}
+					else
+					{
+						/* Keep a count of the bytes received so far. */
+						xReceivedBytes += xReturned;
+					}
+				}
+
+				/* If an error occurred it will be latched in xReceivedBytes,
+				otherwise xReceived bytes will be just that - the number of
+				bytes received from the echo server. */
+				if( xReceivedBytes > 0 )
+				{
+					/* Compare the transmitted string to the received string. */
+					//configASSERT( strncmp( pcReceivedString, pcTransmittedString, lTransmitted ) == 0 );
+					if( strncmp( pcReceivedString, pcTransmittedString, lTransmitted ) == 0 )
+					{
+						/* The echo reply was received without error. */
+						ulTxRxCycles[ xInstance ]++;
+					}
+					else
+					{
+						/* The received string did not match the transmitted
+						string. */
+						ulTxRxFailures[ xInstance ]++;
+						break;
+					}
+				}
+				else if( xReceivedBytes < 0 )
+				{
+					/* FreeRTOS_recv() returned an error. */
+					break;
+				}
+				else
+				{
+					/* Timed out without receiving anything? */
+					break;
+				}
+			}
+
+			/* Finished using the connected socket, initiate a graceful close:
+			FIN, FIN+ACK, ACK. */
+			FreeRTOS_shutdown( xSocket, FREERTOS_SHUT_RDWR );
+
+			/* Expect FreeRTOS_recv() to return an error once the shutdown is
+			complete. */
+			xTimeOnEntering = xTaskGetTickCount();
+			do
+			{
+				xReturned = FreeRTOS_recv( xSocket,	/* The socket being received from. */
+					&( pcReceivedString[ 0 ] ),		/* The buffer into which the received data will be written. */
+					echoBUFFER_SIZES,				/* The size of the buffer provided to receive the data. */
+					0 );
+
+				if( xReturned < 0 )
+				{
+					break;
+				}
+
+			} while( ( xTaskGetTickCount() - xTimeOnEntering ) < xReceiveTimeOut );
+		}
+
+		/* Close this socket before looping back to create another. */
+		FreeRTOS_closesocket( xSocket );
+
+		/* Pause for a short while to ensure the network is not too
+		congested. */
+		vTaskDelay( echoLOOP_DELAY );
+	}
+}
+
+
+#endif /* USE_TCP_ZERO_COPY */
+
 /*-----------------------------------------------------------*/
+
 #endif /* (ipconfigUSE_TCP != 0) */
