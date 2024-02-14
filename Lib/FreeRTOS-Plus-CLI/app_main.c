4de14fdebdaba982e949f4256e70c9e2b8299058
tony-josi-aws <tonyjosi@amazon.com>
2024-02-14 16:19:32 +0530

    Remove test code

--- Lib/FreeRTOS-Plus-CLI/app_main.c
+++ Lib/FreeRTOS-Plus-CLI/app_main.c
@@ -60,7 +60,7 @@
 
 #define USE_IPv6_END_POINTS                 0
 
-#define USE_UDP			 		     		0
+#define USE_UDP			 		     		1
 
 #define USE_TCP			 		     		1
 
@@ -261,7 +261,7 @@ void app_main( void )
                 #if ( ipconfigUSE_DHCP != 0 )
                 {
                     /* End-point 0 wants to use DHCPv4. */
-                    xEndPoints[xEndPointCount].bits.bWantDHCP = pdTRUE; // pdFALSE; // pdTRUE;
+                    xEndPoints[xEndPointCount].bits.bWantDHCP = pdFALSE; // pdFALSE; // pdTRUE;
                 }
                 #endif /* ( ipconfigUSE_DHCP != 0 ) */
 
@@ -974,7 +974,7 @@ static BaseType_t prvIsValidRequest( const uint8_t * pucPacket, uint32_t ulPacke
             * First obtain a buffer of adequate length from the TCP/IP stack into which
             the string will be written. */
     #if defined(ipconfigIPv4_BACKWARD_COMPATIBLE) && ( ipconfigIPv4_BACKWARD_COMPATIBLE == 0 )
-
+		#if 0
         	/*Test to validate if adding 2 internally to requested size cause an overflow in pxGetNetworkBufferWithDescriptor */
         	uint8_t *pucBuffer = FreeRTOS_GetUDPPayloadBuffer_Multi( SIZE_MAX - sizeof( UDPPacket_t ) - 1, portMAX_DELAY, ipTYPE_IPv4 );
         	configASSERT(pucBuffer == NULL);
@@ -991,15 +991,15 @@ static BaseType_t prvIsValidRequest( const uint8_t * pucPacket, uint32_t ulPacke
         	uint8_t *pucBuffer2 = pxResizeNetworkBufferWithDescriptor(pucBuffer, SIZE_MAX - ipBUFFER_PADDING + 1);
         	configASSERT(pucBuffer2 == NULL);
         	//FreeRTOS_ReleaseUDPPayloadBuffer(pucBuffer);
-
-            //pucBuffer = FreeRTOS_GetUDPPayloadBuffer_Multi( sizeof( PacketHeader_t ), portMAX_DELAY, ipTYPE_IPv4 );
+		#endif
+        	uint8_t *pucBuffer = FreeRTOS_GetUDPPayloadBuffer_Multi( sizeof( PacketHeader_t ), portMAX_DELAY, ipTYPE_IPv4 );
     #else
             uint8_t *pucBuffer = FreeRTOS_GetUDPPayloadBuffer( sizeof( PacketHeader_t ), portMAX_DELAY );
     #endif
             configASSERT( pucBuffer != NULL );
             memcpy( pucBuffer , &header, sizeof( PacketHeader_t ) );
 
-            vTaskSuspendAll();
+
             time_check = ARM_REG_DWT_CYCCNT;
             lBytesSent = FreeRTOS_sendto( xCLIServerSocket,
                                         ( void * ) ( pucBuffer ),
@@ -1008,7 +1008,7 @@ static BaseType_t prvIsValidRequest( const uint8_t * pucPacket, uint32_t ulPacke
                                         pxSourceAddress,
                                         xSourceAddressLength );
             time_check = ARM_REG_DWT_CYCCNT - time_check;
-            xTaskResumeAll();
+
 
     #else
 
